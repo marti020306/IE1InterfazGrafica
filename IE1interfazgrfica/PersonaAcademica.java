e8819993d1ee519e53af52cc464222acf8c6776a
Martina Roldan <laramartinaroldan@gmail.com>
2026-05-08 18:47:08 -0300

    Agregue Codigo a la clase

---
+++ IE1interfazgrfica/PersonaAcademica.java
@@ -0,0 +1,46 @@
+package IE1interfazgrfica;
+
+public abstract class PersonaAcademica {
+    private String nombre;
+    private String legajo;
+
+   public PersonaAcademica(String nombre, String legajo){
+
+        setNombre(nombre);
+        setLegajo(legajo);
+   }
+
+   public String getNombre() {
+    
+        return nombre;
+   }
+
+   public void setNombre(String nombre){
+        
+        if (nombre == null || nombre.isEmpty()) {
+
+            throw new IllegalArgumentException("Nombre Invalido");
+           
+        }
+        this.nombre = nombre;
+   }
+
+   public String getLegajo() {
+        
+      return legajo;
+   }
+
+   public void setLegajo(String Legajo){
+        
+        if (legajo ==null){
+
+            throw new IllegalArgumentException("Legajo Invalido");
+        }
+        this.legajo = Legajo; 
+   }
+   
+   public abstract void mostrarResumen();
+
+}
+
+   
