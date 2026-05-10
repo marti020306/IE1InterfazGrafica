0748ad6555bb0c80434366f681cba8daa080a1a3
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2026-05-10 14:13:04 -0300

    Creación de clase Estudiante

---
+++ Estudiante.java
@@ -0,0 +1,17 @@
+package ie1interfazgrafica;
+import java.util.ArrayList;
+
+public class Estudiante extends PersonaAcademica implements Consultable
+{
+    private String carrera;
+    private int anioIngreso;
+    private ArrayList<InscripcionMateria>materias;
+
+    public Estudiante(String nombre, String legajo, String carrera, int anioIngreso)
+    {
+        super(nombre, legajo);
+        setCarrera(carrera);
+        setAnioIngreso(anioIngreso);
+        this.materias = new ArrayList<>();
+    }
+}
