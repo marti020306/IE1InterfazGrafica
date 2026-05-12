5e16b4ef197009de46e334d2541b30491be345ac
Leandro <Gaidoleandro10@gmail.com>
2026-05-12 12:19:09 -0300

    RAMA TERMINADA

--- IE1interfazgrafica/Autogestion.java
+++ IE1interfazgrafica/Autogestion.java
@@ -11,7 +11,7 @@ public class Autogestion
         int opcion;
         do
         {
-            System.out.println("/n===MENU===");
+            System.out.println("\n===MENU===");
             System.out.println("1. Ver Perfil");
             System.out.println("2. Inscribirse a Materia");
             System.out.println("3. Registrar Asistencia");
@@ -24,56 +24,66 @@ public class Autogestion
 
             switch (opcion)
             {
-                 case 1 -> alumno.mostrarResumen();
-                 case 2 -> { System.out.print("Nombre:");
-                 String nombre = sc.nextLine();
-                 System.out.print("Codigo:");
-                 String codigo = sc.nextLine();
-                 System.out.print("Cuatrimestre:");
-                 int cuatri = sc.nextInt();
-                 System.out.print("Año:");
-                 int anio = sc.nextInt();
-                 Materia m = new Materia(nombre, codigo, cuatri, anio);
-                 alumno.inscribirse(m);
-                 case 3 -> {System.out.print("Codigo materia: ");
-                 String cod = sc.nextLine();
-                 InscripcionMateria i = alumno.getInscripcion(cod);
+                 case 1: alumno.mostrarResumen();
+                    break;
+
+                     case 2: { System.out.print("Nombre:");
+                     String nombre = sc.nextLine();
+                     System.out.print("Codigo:");
+                     String codigo = sc.nextLine();
+                     System.out.print("Cuatrimestre:");
+                     int cuatri = sc.nextInt();
+                     System.out.print("Año:");
+                     int anio = sc.nextInt();
+                     Materia m = new Materia(nombre, codigo, cuatri, anio);
+                     alumno.inscribirse(m); }
+                        break;
+
+                        case 3: {System.out.print("Codigo materia: ");
+                         String cod = sc.nextLine();
+                        InscripcionMateria i = alumno.getInscripcion(cod);
                   
-                   if (i != null)
-                   {System.out.print("¿Presente? (true/false): ");
-                   boolean p = sc.nextBoolean();
-                   sc.nextLine();
-                   i.registrarAsistencia(p);
-                   }
-                   
-                       }
-                 case 4 -> {System.out.print("Codigo materia: ");
-                    String cod = sc.nextLine();
-                    InscripcionMateria i = alumno.getInscripcion(cod);
-                    if (i != null)
-                    {
-                        System.out.print("Nota: ");
-                        double nota = sc.nextDouble();
+                        if (i != null)
+                        {System.out.print("¿Presente? (true/false): ");
+                         boolean p = sc.nextBoolean();
                         sc.nextLine();
-                        i.agregarNota(nota);
-                    }
-                          }
-                 case 5 -> { for (InscripcionMateria i: alumno.getMaterias());
-                    { 
-                        System.out.println(i.getMateria().getNombre());
-                        i.mostrarEstadoAcademico();
-                    }
-  
+                        i.registrarAsistencia(p);
+                        }
+                        }
+                        break;
 
-                    }
-                 case 0 -> System.out.println("Chau!");
-                 default -> System.out.println("Opcion Invalida");
+                             case 4: {System.out.print("Codigo materia: ");
+                             String cod = sc.nextLine();
+                             InscripcionMateria i = alumno.getInscripcion(cod);
+                             if (i != null)
+                            {
+                             System.out.print("Nota: ");
+                             double nota = sc.nextDouble();
+                             sc.nextLine();
+                             i.agregarNota(nota);
+                            }
+                        }
+                        break;
 
+                            case 5: { for (InscripcionMateria i: alumno.getMaterias())
+                            { 
+                                System.out.println(i.getMateria().getNombre());
+                                i.mostrarEstadoAcademico();
+                            }
+                        }
+                        break;
+                           
+                    case 0: System.out.println("Chau!");
+                    break;
+
+                    default: System.out.println("Opcion Invalida");
+                    break;    
 
                 }
             } while (opcion != 0);
+              sc.close();
         }
     }   
 
 
-}
+
